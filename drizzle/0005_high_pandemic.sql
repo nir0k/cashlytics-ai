3f9232f54239972e65a7fb48c9e40cfe7bfe2c52
default <admin@roennevej119.online>
2026-02-25 14:27:13 +0000

    fix: solve migration issue for users

--- drizzle/0005_high_pandemic.sql
+++ drizzle/0005_high_pandemic.sql
@@ -1,3 +1,11 @@
+DELETE FROM "accounts" WHERE "user_id" IS NULL;--> statement-breakpoint
+DELETE FROM "categories" WHERE "user_id" IS NULL;--> statement-breakpoint
+DELETE FROM "conversations" WHERE "user_id" IS NULL;--> statement-breakpoint
+DELETE FROM "daily_expenses" WHERE "user_id" IS NULL;--> statement-breakpoint
+DELETE FROM "documents" WHERE "user_id" IS NULL;--> statement-breakpoint
+DELETE FROM "expenses" WHERE "user_id" IS NULL;--> statement-breakpoint
+DELETE FROM "incomes" WHERE "user_id" IS NULL;--> statement-breakpoint
+DELETE FROM "transfers" WHERE "user_id" IS NULL;--> statement-breakpoint
 ALTER TABLE "accounts" ALTER COLUMN "user_id" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "categories" ALTER COLUMN "user_id" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "conversations" ALTER COLUMN "user_id" SET NOT NULL;--> statement-breakpoint
